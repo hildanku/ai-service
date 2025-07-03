cbfa62df0ceaba2d0104dd0e103dabe4192e4393
hildanku <hildankutomo@gmail.com>
2025-07-03 16:54:48 +0700

    feat: add classification feature

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import { GoogleGenAI, Models } from '@google/genai'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { Buffer } from 'buffer'
+import { appResponse } from './lib/response.js'
+
+const ai = new GoogleGenAI({ apiKey: '' })
 
 const app = new Hono()
 
@@ -8,14 +13,40 @@ app.get('/api/healthcheck', (c) => {
 })
 
 app.post('/api/upload', async (c) => {
-    const body = await c.req.parseBody()
-    const data = body['file']
-    return c.json({
-        message: 'File received successfully!',
-        fileName: data.name,
-        fileSize: data.size,
-        fileType: data.type
-    })
+    try {
+        const formData = await c.req.formData()
+        const file = formData.get('file') as FormDataEntryValue
+
+        if (!file || !(file instanceof File)) {
+            // return c.json({ error: 'No file uploaded or invalid file format' }, 400)
+            return appResponse(c, 'No file uploaded or invalid file format', 400, null)
+        }
+        const buffer = await file.arrayBuffer()
+        const base64String = Buffer.from(buffer).toString('base64')
+
+        const promptConfig = [
+            { text: "Identify the type of building material in this image and describe it in no more than 3 words. Please answer in Bahasa Indonesia." },
+            {
+                inlineData: {
+                    mimeType: "image/jpeg",
+                    data: base64String,
+                },
+            },
+        ]
+
+        const result = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: promptConfig,
+        })
+
+        const aiDescription = result.text
+
+        return appResponse(c, 'Image processed successfully!', 200, { description: aiDescription })
+
+    } catch (error) {
+        console.error('Error processing image:', error)
+        return appResponse(c, 'Failed to process image', 500, null)
+    }
 })
 
 serve({
